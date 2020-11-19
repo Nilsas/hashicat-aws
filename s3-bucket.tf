a75700c8bac7858795304c17f9db1ef1ea0aec12
Nilsas <n.firantas@gmail.com>
2020-11-19 14:30:34 +0200

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucker_prefix = var.prefix
-  bucket        = "my-s3-bucket"
-  acl           = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/nafiCorp/s3-bucket/aws"
+  version = "1.16.0"
 }
+
